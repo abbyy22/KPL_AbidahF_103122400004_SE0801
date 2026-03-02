0dc1a783967b179e30dac2bc3a04c9695e6a7b76
Abidah Fatimatuzzahrah <abidahh225@gmail.com>
2026-03-03 00:23:36 +0700

    add TP

---
+++ 02_Pemograman_JavaScript/TP_02_Pemograman_JavaScript.md
@@ -0,0 +1,16 @@
+# Tugas TP 02: Pemograman JavaScript
+
+**Soal**
+Kamu sudah menulis fungsi mulOfArray. Ujilah dengan input [2, 0, 26, 28, -2], dengan output yang seharusnya adalah 1456. Jika kamu menemukan bahwa hasilnya berbeda, bisakah kamu memperbaikinya? Jika kamu menemukan bahwa hasilnya sama, bisakah kamu menjelaskan mengapa demikian?
+
+**Code Sumber**
+Tersedia di [index.js](./index.js)
+
+**Output**
+Sudah sesuai, 
+PS D:\KPL\KPL_AbidahF_103122400004_SE0801\02_Pemograman_JavaScript> node index.js
+15
+1456
+
+**Deskripsi Program**
+Program ini menjalankan perkalian semua bilangan positif dalam array. Ini akan bekerja untuk bilangan positif, nol, dan negatif sesuai dengan perintahnya.
--- 02_Pemograman_JavaScript/index.js
+++ 02_Pemograman_JavaScript/index.js
@@ -1,11 +1,13 @@
+//coba sesuai di tp
+
 const arr1 = [1, -2, 3, -4, 5, -6];
 
 function mulOfArray(arr) {
     let result = 1;
-    for (let i = 0; i < arr.length; i = i + 1) {
-       if (arr[i] >= 0) {
-           result = result * arr[i];
-       }
+    for (let i = 0; i < arr.length; i = i + 1) { //looping untuk mengakses setiap elemen dalam array
+       if (arr[i] >= 0) { //kondisi untuk memeriksa apakah elemen tersebut lebih besar atau sama dengan 0
+           result = result * arr[i]; //jika kondisi terpenuhi, maka hasil perkalian akan disimpan dalam variabel result
+       } // 1*3*5 = 15, yang minus gabole ikut dikaliin, soalnya peraturannya >= 0
     }
     
     return result;
@@ -14,3 +16,22 @@ function mulOfArray(arr) {
 // Panggil fungsinya
 const arr1Result = mulOfArray(arr1);
 console.log(arr1Result);
+
+// soal
+
+const arr2 = [2, 0, 26, 28, -2];
+
+function mulOfArray(arr) {
+    let result = 1;
+    for (let i = 0; i < arr.length; i = i + 1) {
+       if (arr[i] > 0) { //bedanya disini, 0 nya jangan diikutin biar hasilnya ga 0. soalnya semua yg dikaliin 0 = 0
+           result = result * arr[i];
+       } // 2*26*28 = 1456, yang minus dan 0 gabole ikut dikaliin, soalnya peraturannya > 0
+    }
+    
+    return result;
+}
+
+// Panggil fungsinya
+const arr2Result = mulOfArray(arr2);
+console.log(arr2Result);
